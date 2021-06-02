bf1b764751c6cc40c3e6f83afd6cc5859968a5c7
gabriellpa <gabriellpantunes@gmail.com>
2021-06-01 22:00:43 -0300

    adicionado cors para reDoc

---
+++ src/main/java/com/voting/sessions/config/CorsConfiguration.java
@@ -0,0 +1,21 @@
+package com.voting.sessions.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Value("${origin:http://localhost:3000}")
+    private String origin;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(origin)
+                .allowedMethods("GET");
+    }
+
+}
